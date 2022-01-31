7a95a0d4cb632fcda718bc09691a19b53cbc995b
Bulaev Alexander <avlabulaev@sberbank.ru>
2022-01-31 03:59:42 +0300

    TabManager теперь правильно синхронизируется

--- packages/front/src/modules/body/Body.tsx
+++ packages/front/src/modules/body/Body.tsx
@@ -4,19 +4,20 @@ import { TabHeader } from '@modules/body/TabHeader'
 import { BodyPathSwitcher } from '@modules/body/BodyPathSwitcher/BodyPathSwitcher'
 
 export const Body = () => {
-  const { dispatch } = useContext(MyContext)
+  const { state, dispatch } = useContext(MyContext)
   const request = Request({
     url: 'api/data',
     payload: {},
     dispatch,
   })
+  const pageNumber = state.appInfo.tab
   useEffect(() => {
     request()
   }, [])
   return (
     <div>
       <TabHeader />
-      <BodyPathSwitcher />
+      {pageNumber === 3 ? <>TODOSETTING PAGE</> : <BodyPathSwitcher />}
     </div>
   )
 }
--- packages/front/src/modules/body/TabHeader.tsx
+++ packages/front/src/modules/body/TabHeader.tsx
@@ -5,6 +5,18 @@ import { MyContext } from '@context'
 export const TabHeader = () => {
   const { state, dispatch } = useContext(MyContext)
   const activeTab = state.appInfo.tab
+
+  const getClassName = (tabNumber: number) => {
+    const tab = state.pathInfo[tabNumber]
+    if (!tab.connected) {
+      return `${style.nav_item} ${style.brown_lamp}`
+    }
+
+    return tab.status === 'PROGRESS'
+      ? `${style.nav_item} ${style.green_lamp}`
+      : `${style.nav_item} ${style.yellow_lamp}`
+  }
+
   const tabs: {
     current: boolean
     id: 0 | 1 | 2 | 3
@@ -13,19 +25,19 @@ export const TabHeader = () => {
   }[] = [
     {
       id: 0,
-      cn: `${style.nav_item} ${style.green_lamp}`,
+      cn: getClassName(0),
       status: false,
       current: false,
     },
     {
       id: 1,
-      cn: `${style.nav_item} ${style.yellow_lamp}`,
+      cn: getClassName(1),
       status: false,
       current: false,
     },
     {
       id: 2,
-      cn: `${style.nav_item} ${style.brown_lamp}`,
+      cn: getClassName(2),
       status: false,
       current: false,
     },
@@ -41,7 +53,6 @@ export const TabHeader = () => {
     tab.current = tab.id === activeTab
   })
 
-  // Dispatch({ type: 'changePage', page: 1 });
   return (
     <div className={style.nav_container}>
       {tabs.map((tab, index) => (
@@ -51,9 +62,7 @@ export const TabHeader = () => {
           onClick={() => {
             dispatch({ type: 'changePage', page: tab.id })
           }}
-        >
-          {index}
-        </div>
+        />
       ))}
     </div>
   )
