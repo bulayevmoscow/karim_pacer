face5782fbe36cab6f000c0e2be85bab7e242340
Alexander Bulaev <alexzlow@gmail.com>
2022-02-19 19:03:53 +0300

    Шаблон тела

--- packages/front/src/modules/components/tracks/Tracks.tsx
+++ packages/front/src/modules/components/tracks/Tracks.tsx
@@ -1,56 +1,61 @@
 import { observer } from 'mobx-react-lite'
 import { useTracks } from '@modules/components/tracks/useTracks'
-import { BodyTemplate } from '@modules/library/templates/bodyTemplate/BodyTemplate'
-import { TrackUnit } from '@modules/components/tracks/unit/TrackUnit'
+import BodyTemplate from '@modules/library/templates/bodyTemplate/BodyTemplate'
 import { Button } from '@modules/library/Button'
 import React from 'react'
+import { TrackUnit } from '@modules/components/tracks/unit/TrackUnit'
 
 export const Tracks = observer(() => {
   const { setShowButtonPanel, showButtonPanel, lanesInfo, eventStart, goToLane, startInterval } = useTracks()
   console.log(JSON.parse(JSON.stringify(lanesInfo)))
-  const ButtonPanel = (
-    <>
-      <Button
-        color="red"
-        onClick={e => {
-          e.stopPropagation()
-        }}
-      >
-        Отключить
-      </Button>
-      <Button
-        color="green"
-        onClick={e => {
-          e.stopPropagation()
-        }}
-      >
-        Включить
-      </Button>
-      <Button
-        color="gray"
-        onClick={e => {
-          e.stopPropagation()
-        }}
-      >
-        Редактировать
-      </Button>
-    </>
-  )
+  const ButtonPanel = <></>
 
   return (
-    <BodyTemplate buttonPanel={ButtonPanel}>
-      {lanesInfo?.length > 0 &&
-        lanesInfo.map(lane => (
-          <TrackUnit
-            key={lane.id}
-            name={lane.name ?? 'Дорожка'}
-            isDiconnected={!lane.connected}
-            {...lane.intervals[0]}
-            progress={lane.progress}
-            onClick={eventStart(() => (showButtonPanel === lane.id ? undefined : setShowButtonPanel(lane.id)))}
-            isClick={lane.id === showButtonPanel}
-          />
-        ))}
-    </BodyTemplate>
+    <BodyTemplate.Container>
+      <BodyTemplate.Main>
+        {lanesInfo?.length > 0 &&
+          lanesInfo.map(lane => (
+            <TrackUnit
+              key={lane.id}
+              name={lane.name ?? 'Дорожка'}
+              isDiconnected={!lane.connected}
+              {...lane.intervals[0]}
+              progress={lane.progress}
+              onClick={eventStart(() => (showButtonPanel === lane.id ? undefined : setShowButtonPanel(lane.id)))}
+              isClick={lane.id === showButtonPanel}
+            />
+          ))}
+      </BodyTemplate.Main>
+      <BodyTemplate.Buttons>
+        <Button
+          color="red"
+          onClick={e => {
+            e.stopPropagation()
+          }}
+        >
+          Отключить
+        </Button>
+        <Button
+          color="green"
+          onClick={e => {
+            e.stopPropagation()
+          }}
+        >
+          Включить
+        </Button>
+        <Button
+          color="gray"
+          onClick={e => {
+            e.stopPropagation()
+          }}
+        >
+          Редактировать
+        </Button>
+      </BodyTemplate.Buttons>
+    </BodyTemplate.Container>
   )
 })
+
+//    <BodyTemplate buttonPanel={ButtonPanel}>
+
+//     </BodyTemplate>
--- packages/front/src/modules/library/templates/bodyTemplate/BodyTemplate.tsx
+++ packages/front/src/modules/library/templates/bodyTemplate/BodyTemplate.tsx
@@ -1,20 +1,37 @@
 import style from './BodyTemplate.module.scss'
 import React, { FC } from 'react'
 
-type TBodyTemplate = {
-  buttonPanel?: React.ReactNode
-  children: React.ReactNode
+const BodyButtonTemplate: FC = ({ children }) => {
+  return (
+    <div className={style.tracks_buttons_container}>
+      <div className={style.body_template_buttons_container}>{children}</div>
+    </div>
+  )
 }
 
-export const BodyTemplate: FC<TBodyTemplate> = ({ children, buttonPanel }) => {
+const BodyMainTemplate: FC = ({ children }) => {
   return (
-    <div className={style.body_template}>
-      <div className={style.body_template_container}>
-        <div className={style.body_template_list}>{children}</div>
-      </div>
-      <div className={style.tracks_buttons_container}>
-        <div className={style.body_template_buttons_container}>{buttonPanel}</div>
+    <div className={style.body_template_container}>
+      <div>
+        {children}
+        {children}
+        {children}
+        {children}
+        {children}
+        {children}
+        {children}
+        {children}
       </div>
     </div>
   )
+}
+
+const BodyTemplate: FC = ({ children }) => {
+  return <div className={style.body_template}>{children}</div>
+}
+
+export default {
+  Container: BodyTemplate,
+  Main: BodyMainTemplate,
+  Buttons: BodyButtonTemplate,
 }
